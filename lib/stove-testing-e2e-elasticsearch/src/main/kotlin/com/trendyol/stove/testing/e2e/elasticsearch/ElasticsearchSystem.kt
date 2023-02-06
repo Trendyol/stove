6b1e436e71d8452c3bec4c5c0ede627ecae428b8
vladyslav.bozhenko <vladyslav.bozhenko@trendyol.com>
2023-02-06 09:42:47 +0200

    elasticsearch support added

--- lib/stove-testing-e2e-elasticsearch/src/main/kotlin/com/trendyol/stove/testing/e2e/elasticsearch/ElasticsearchSystem.kt
+++ lib/stove-testing-e2e-elasticsearch/src/main/kotlin/com/trendyol/stove/testing/e2e/elasticsearch/ElasticsearchSystem.kt
@@ -3,6 +3,8 @@
 package com.trendyol.stove.testing.e2e.elasticsearch
 
 import arrow.core.getOrElse
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.trendyol.stove.testing.e2e.containers.DEFAULT_REGISTRY
 import com.trendyol.stove.testing.e2e.containers.withProvidedRegistry
 import com.trendyol.stove.testing.e2e.database.DatabaseSystem
 import com.trendyol.stove.testing.e2e.elasticsearch.ElasticsearchSystem.Constants.ONE_MINUTE
@@ -16,15 +18,14 @@ import com.trendyol.stove.testing.e2e.system.abstractions.SystemNotRegisteredExc
 import org.apache.http.HttpHost
 import org.apache.http.client.config.RequestConfig.Builder
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
-import org.elasticsearch.action.DocWriteRequest
+import org.elasticsearch.action.delete.DeleteRequest
 import org.elasticsearch.action.get.GetRequest
 import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.Request
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestClientBuilder
 import org.elasticsearch.client.RestHighLevelClient
-import org.elasticsearch.client.indices.CreateIndexRequest
-import org.elasticsearch.common.xcontent.XContentFactory
 import org.elasticsearch.common.xcontent.XContentType
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -37,17 +38,18 @@ data class ElasticsearchSystemOptions(
 )
 
 fun TestSystem.withElasticsearch(
+    index: String,
     registry: String = "docker.elastic.co/",
-    // options: ElasticsearchSystemOptions = ElasticsearchSystemOptions(),
+    imageVersion: String = "7.10.2",
+    options: ElasticsearchSystemOptions = ElasticsearchSystemOptions(),
 ): TestSystem {
     val elasticsearchContainer =
-        withProvidedRegistry("elasticsearch/elasticsearch:7.10.2", registry) {
+        withProvidedRegistry("elasticsearch/elasticsearch:$imageVersion", registry) {
             ElasticsearchContainer(it)
         }
     elasticsearchContainer.addExposedPorts(9200)
-    // elasticsearchContainer.withPassword("s3cret")
     this.getOrRegister(
-        ElasticsearchSystem(this, ElasticsearchContext(elasticsearchContainer, ElasticsearchSystemOptions()))
+        ElasticsearchSystem(this, ElasticsearchContext(index, elasticsearchContainer, options))
     )
     return this
 }
@@ -55,12 +57,10 @@ fun TestSystem.withElasticsearch(
 data class ElasticSearchExposedConfiguration(
     val host: String,
     val port: Int,
-    val username: String,
-    val password: String,
 )
 
 data class ElasticsearchContext(
-    // val bucket: BucketDefinition,
+    val index: String,
     val container: ElasticsearchContainer,
     val options: ElasticsearchSystemOptions,
 )
@@ -74,14 +74,11 @@ class ElasticsearchSystem internal constructor(
     override val testSystem: TestSystem,
     private val context: ElasticsearchContext,
 ) : DatabaseSystem, RunAware, ExposesConfiguration {
-
     private lateinit var client: RestHighLevelClient
 
-    // private lateinit var collection: ReactiveCollection
     private lateinit var exposedConfiguration: ElasticSearchExposedConfiguration
     private val objectMapper: StoveJsonSerializer = context.options.jsonSerializer
 
-    // private val objectMapper: StoveJsonSerializer = context.options.jsonSerializer
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     object Constants {
@@ -93,109 +90,68 @@ class ElasticsearchSystem internal constructor(
         context.container.start()
         exposedConfiguration = ElasticSearchExposedConfiguration(
             context.container.host,
-            context.container.firstMappedPort,
-            "elastic",
-            "s3cret"
+            context.container.firstMappedPort
         )
         client = createCluster(exposedConfiguration)
-        // collection = createDefaultCollection()
     }
 
     override suspend fun stop() {
         context.container.stop()
     }
 
-    // override fun configuration(): List<String> {
-    //     // return context.options.configureExposedConfiguration(exposedConfiguration) +
-    //     //     listOf(
-    //     //         "couchbase.hosts=${exposedConfiguration.hostsWithPort}",
-    //     //         "couchbase.username=${context.container.username}",
-    //     //         "couchbase.password=${context.container.password}"
-    //     //     )
-    // }
-
-    // override suspend fun <T : Any> shouldQuery(
-    //     query: String,
-    //     assertion: (List<T>) -> Unit,
-    //     clazz: KClass<T>,
-    // ): ElasticsearchSystem {
-    //     val result = cluster.executeQueryAs<Any>(query) { queryOptions -> queryOptions.scanConsistency(REQUEST_PLUS) }
-    //
-    //     val objects = result
-    //         .map { objectMapper.serialize(it) }
-    //         .map { objectMapper.deserialize(it, clazz) }
-    //
-    //     assertion(objects)
-    //     return this
-    // }
-
-    fun <T : Any> shouldGets(
-        indexName: String,
+    fun <T : Any> save(
         id: String,
-        clazz: KClass<T>,
-        assertion: (T) -> Unit,
+        instance: T,
     ): ElasticsearchSystem {
-        val result = client.get(GetRequest(indexName, id), RequestOptions.DEFAULT)
+        val request = IndexRequest(context.index).id(id)
+        val jsonInstance = objectMapper.serialize(instance)
+        request.source(jsonInstance, XContentType.JSON)
+        client.index(request, RequestOptions.DEFAULT)
+        return this
+    }
+
+    override suspend fun <T : Any> shouldQuery(
+        query: String,
+        assertion: (List<T>) -> Unit,
+        clazz: KClass<T>,
+    ): DatabaseSystem {
+        val request = Request("GET", "${context.index}/_search")
+        val response = client.lowLevelClient.performRequest(request)
+        val jsonNode = ObjectMapper().readTree(response.entity.content.reader().readText()).get("hits").get("hits")
+        val queryResults = jsonNode.map {
+            objectMapper.deserialize(it.get("_source").toString(), clazz)
+        }
+        assertion(queryResults)
+        return this
+    }
+
+    override suspend fun <T : Any> shouldGet(
+        id: String,
+        assertion: (T) -> Unit,
+        clazz: KClass<T>,
+    ): DatabaseSystem {
+        val result = client.get(GetRequest(context.index, id), RequestOptions.DEFAULT)
         val actual = objectMapper.deserialize(result.sourceAsString, clazz)
         assertion(actual)
 
         return this
     }
-    //
-    // override suspend fun shouldDelete(id: String): ElasticsearchSystem {
-    //     collection.remove(id).awaitSingle()
-    //     return this
-    // }
-    //
-    // /**
-    //  * Saves the [instance] with given [id] to the [collection]
-    //  * To save to the default collection use [saveToDefaultCollection]
-    //  */
-    // suspend fun <T : Any> save(
-    //     collection: String,
-    //     id: String,
-    //     instance: T,
-    // ): ElasticsearchSystem {
-    //     cluster
-    //         .bucket(context.bucket.name)
-    //         .collection(collection)
-    //         .insert(
-    //             id,
-    //             JsonObject.fromJson(objectMapper.serialize(instance)),
-    //             InsertOptions.insertOptions().durability(PERSIST_TO_MAJORITY)
-    //         )
-    //         .awaitSingle()
-    //
-    //     return this
-    // }
-    //
-    // /**
-    //  * Saves the [instance] with given [id] to the default collection
-    //  * In couchbase the default collection is `_default`
-    //  */
-    // suspend inline fun <reified T : Any> saveToDefaultCollection(
-    //     id: String,
-    //     instance: T,
-    // ): ElasticsearchSystem = this.save("_default", id, instance)
-    //
-    // override fun close(): Unit = runBlocking {
-    //     Try { cluster.disconnect().awaitSingle() }.recover {
-    //         logger.warn("Disconnecting the couchbase cluster got an error: $it")
-    //     }
-    // }
 
-    // private fun createDefaultCollection(): ReactiveCollection = cluster.bucket(context.bucket.name).defaultCollection()
-
-    fun <T : Any> save(
-        index: String,
-        id: String,
-        instance: T,
-    ): ElasticsearchSystem {
-        val request = IndexRequest(index).id(id)
-        val jsonString = objectMapper.serialize(instance)
-        request.source(jsonString, XContentType.JSON)
-        client.index(request, RequestOptions.DEFAULT)
+    override suspend fun shouldDelete(id: String): DatabaseSystem {
+        client.delete(DeleteRequest(context.index, id), RequestOptions.DEFAULT)
         return this
+    }
+
+    override fun close() {
+        client.close()
+    }
+
+    override fun configuration(): List<String> {
+        return context.options.configureExposedConfiguration(exposedConfiguration) +
+            listOf(
+                "elasticsearch.host=${exposedConfiguration.host}",
+                "elasticsearch.port=${exposedConfiguration.port}"
+            )
     }
 
     private fun createCluster(exposedConfiguration: ElasticSearchExposedConfiguration): RestHighLevelClient {
@@ -212,44 +168,6 @@ class ElasticsearchSystem internal constructor(
                 config
             }
             .setRequestConfigCallback(requestConfigCallback)
-        //        builder.setMaxRetryTimeoutMillis(ONE_MINUTE);
         return RestHighLevelClient(builder)
     }
-
-    override suspend fun <T : Any> shouldQuery(
-        query: String,
-        assertion: (List<T>) -> Unit,
-        clazz: KClass<T>,
-    ): DatabaseSystem {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun <T : Any> shouldGet(
-        id: String,
-        assertion: (T) -> Unit,
-        clazz: KClass<T>,
-    ): DatabaseSystem {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun shouldDelete(id: String): DatabaseSystem {
-        TODO("Not yet implemented")
-    }
-
-    override fun close() {
-        client.close()
-    }
-
-    override fun configuration(): List<String> {
-        return listOf()
-    }
-
-    // private fun jsonSerializer(): JsonSerializer = object : JsonSerializer {
-    //     override fun serialize(input: Any): ByteArray = objectMapper.serializeAsBytes(input)
-    //
-    //     override fun <T : Any> deserialize(
-    //         target: Class<T>,
-    //         input: ByteArray,
-    //     ): T = objectMapper.deserialize(input, Reflection.getOrCreateKotlinClass(target)) as T
-    // }
 }
