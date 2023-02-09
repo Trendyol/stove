222fc7a057d71b31b65a5eec8d73d0421fe0ff87
vladyslav.bozhenko <vladyslav.bozhenko@trendyol.com>
2023-02-09 12:52:45 +0200

    add search test

--- lib/stove-testing-e2e-elasticsearch/src/test/kotlin/com/trendyol/stove/testing/e2e/elasticsearch/ElasticsearchTestSystemTests.kt
+++ lib/stove-testing-e2e-elasticsearch/src/test/kotlin/com/trendyol/stove/testing/e2e/elasticsearch/ElasticsearchTestSystemTests.kt
@@ -1,6 +1,7 @@
 package com.trendyol.stove.testing.e2e.elasticsearch
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.trendyol.stove.testing.e2e.database.DatabaseSystem.Companion.shouldQuery
 import com.trendyol.stove.testing.e2e.database.DocumentDatabaseSystem.Companion.shouldGet
 import com.trendyol.stove.testing.e2e.system.TestSystem
 import com.trendyol.stove.testing.e2e.system.abstractions.ApplicationUnderTest
@@ -44,6 +45,27 @@ class CouchbaseTestSystemTests : FunSpec({
             .save(exampleInstance.id, exampleInstance)
             .shouldGet<ExampleInstance>(exampleInstance.id) {
                 it.description shouldBe exampleInstance.description
+            }
+    }
+
+    test("should save 2 documents with the same description, then delete first one and query by description") {
+        val desc = "some description"
+        val exampleInstance1 = ExampleInstance("1", desc)
+        val exampleInstance2 = ExampleInstance("2", desc)
+        // val queryByDesc = QueryBuilders.term().field("description")
+        //     .value(exampleInstance2.description)
+        //     .queryName("term_by_description")
+        //     .build()
+
+        TestSystem.instance
+            .elasticsearch()
+            .save(exampleInstance1.id, exampleInstance1)
+            .save(exampleInstance2.id, exampleInstance2)
+            .shouldDelete(exampleInstance1.id)
+            .shouldGet<ExampleInstance>(exampleInstance2.id) {}
+            .shouldQuery<ExampleInstance>("{}") { items ->
+                items.size shouldBe 1
+                items[0] shouldBe exampleInstance2
             }
     }
 })
