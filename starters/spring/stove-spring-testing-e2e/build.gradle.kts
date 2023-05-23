2715f5c077314c7727088878ef309945bcf2b4ea
Oguzhan Soykan <oguzhansoykan@gmail.com>
2023-05-23 20:43:46 +0200

    remove strict versions

--- starters/spring/stove-spring-testing-e2e/build.gradle.kts
+++ starters/spring/stove-spring-testing-e2e/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     api(project(":lib:stove-testing-e2e"))
-    api(libs.spring.framework.context)
+    implementation(libs.spring.framework.context)
     implementation(libs.spring.boot.itself)
 }
