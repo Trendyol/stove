7c9f79710b16e847ce60a443c590be0b014320e2
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-04-30 16:36:27 +0200

    use typed project accessors for spring example

--- examples/spring-example/build.gradle.kts
+++ examples/spring-example/build.gradle.kts
@@ -1,36 +1,35 @@
 plugins {
-    alias(libs.plugins.spring.plugin)
-    alias(libs.plugins.spring.boot)
-    idea
-    application
+  alias(libs.plugins.spring.plugin)
+  alias(libs.plugins.spring.boot)
+  idea
+  application
 }
 
 dependencies {
-    implementation(libs.spring.boot.get3x())
-    implementation(libs.spring.boot.get3x().autoconfigure)
-    implementation(libs.spring.boot.get3x().webflux)
-    implementation(libs.spring.boot.get3x().actuator)
-    annotationProcessor(libs.spring.boot.get3x().annotationProcessor)
-    api(libs.spring.boot.get3x().kafka)
-    implementation(libs.kotlinx.reactor)
-    implementation(libs.kotlinx.core)
-    implementation(libs.kotlinx.reactive)
-    api(libs.couchbase.client)
-    api(libs.couchbase.client.metrics)
-    implementation(libs.jackson.kotlin)
-    implementation(libs.kotlinx.slf4j)
+  implementation(libs.spring.boot.get3x())
+  implementation(libs.spring.boot.get3x().autoconfigure)
+  implementation(libs.spring.boot.get3x().webflux)
+  implementation(libs.spring.boot.get3x().actuator)
+  annotationProcessor(libs.spring.boot.get3x().annotationProcessor)
+  api(libs.spring.boot.get3x().kafka)
+  implementation(libs.kotlinx.reactor)
+  implementation(libs.kotlinx.core)
+  implementation(libs.kotlinx.reactive)
+  api(libs.couchbase.client)
+  api(libs.couchbase.client.metrics)
+  implementation(libs.jackson.kotlin)
+  implementation(libs.kotlinx.slf4j)
 }
 
 dependencies {
-    testImplementation(testLibs.kotest.property.jvm)
-    testImplementation(testLibs.kotest.runner.junit5)
-    testImplementation(project(":lib:stove-testing-e2e"))
-    testImplementation(project(":lib:stove-testing-e2e-http"))
-    testImplementation(project(":lib:stove-testing-e2e-wiremock"))
-    testImplementation(project(":lib:stove-testing-e2e-couchbase"))
-    testImplementation(project(":lib:stove-testing-e2e-elasticsearch"))
-    testImplementation(project(":starters:spring:stove-spring-testing-e2e"))
-    testImplementation(project(":starters:spring:stove-spring-testing-e2e-kafka"))
+  testImplementation(testLibs.kotest.property.jvm)
+  testImplementation(testLibs.kotest.runner.junit5)
+  testImplementation(projects.stove.lib.stoveTestingE2eHttp)
+  testImplementation(projects.stove.lib.stoveTestingE2eWiremock)
+  testImplementation(projects.stove.lib.stoveTestingE2eCouchbase)
+  testImplementation(projects.stove.lib.stoveTestingE2eElasticsearch)
+  testImplementation(projects.stove.starters.spring.stoveSpringTestingE2e)
+  testImplementation(projects.stove.starters.spring.stoveSpringTestingE2eKafka)
 }
 
 application { mainClass.set("stove.spring.example.ExampleAppkt") }
