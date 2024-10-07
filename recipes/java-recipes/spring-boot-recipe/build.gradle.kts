f943382effebf88b18b11d219a6e515ecece08b7
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-10-07 18:28:41 +0200

    fix recipes transitive testcontainers-kafka problem

--- recipes/java-recipes/spring-boot-recipe/build.gradle.kts
+++ recipes/java-recipes/spring-boot-recipe/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
   testImplementation(libs.stove.testing.http)
   testImplementation(libs.stove.testing.wiremock)
   testImplementation(libs.stove.testing.kafka)
+  testImplementation(libs.testcontainers.kafka)
   testImplementation(libs.stove.spring.testing)
   testImplementation(libs.jackson.kotlin)
 }
