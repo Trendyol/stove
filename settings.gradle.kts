c91ea745e5b953dc3728499c337fb99c2214a160
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-02-13 11:41:07 +0100

    add pre-commmit hook

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,3 +35,12 @@ dependencyResolutionManagement {
     }
   }
 }
+plugins {
+  id("org.danilopianini.gradle-pre-commit-git-hooks").version("2.0.20")
+}
+gitHooks {
+  preCommit {
+    from(rootDir.resolve("pre-commit.sh"))
+  }
+  createHooks(overwriteExisting = true)
+}
