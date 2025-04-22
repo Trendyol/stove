bb8694e6f967051cd4809d009d9be2cab75a467f
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-04-22 15:50:04 +0200

    fixes for kotest 6.0.0.M3

--- starters/spring/stove-spring-testing-e2e-kafka/src/test/kotlin/com/trendyol/stove/testing/e2e/kafka/shared.kt
+++ starters/spring/stove-spring-testing-e2e-kafka/src/test/kotlin/com/trendyol/stove/testing/e2e/kafka/shared.kt
@@ -2,10 +2,11 @@ package com.trendyol.stove.testing.e2e.kafka
 
 import io.kotest.common.ExperimentalKotest
 import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.engine.concurrency.SpecExecutionMode
 
 class Setup : AbstractProjectConfig() {
   @ExperimentalKotest
-  override val concurrentSpecs: Int = 1
+  override val specExecutionMode: SpecExecutionMode = SpecExecutionMode.Sequential
 }
 
 class StoveBusinessException(
