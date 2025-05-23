9b9d672807b23c31afda71fdef1f4067a10944f2
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-05-23 15:32:39 +0200

    chore: fix deprecated streamsconfig

--- examples/spring-streams-example/src/main/kotlin/stove/spring/streams/example/kafka/StreamsConfig.kt
+++ examples/spring-streams-example/src/main/kotlin/stove/spring/streams/example/kafka/StreamsConfig.kt
@@ -2,19 +2,10 @@ package stove.spring.streams.example.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG
-import org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG
-import org.apache.kafka.streams.StreamsConfig.COMMIT_INTERVAL_MS_CONFIG
-import org.apache.kafka.streams.StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG
-import org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG
-import org.apache.kafka.streams.StreamsConfig.DEFAULT_PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG
-import org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG
+import org.apache.kafka.streams.StreamsConfig.*
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-import org.springframework.kafka.annotation.EnableKafkaStreams
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration
+import org.springframework.context.annotation.*
+import org.springframework.kafka.annotation.*
 import org.springframework.kafka.config.KafkaStreamsConfiguration
 
 @Configuration
@@ -35,8 +26,8 @@ class StreamsConfig {
     props[DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
     props[DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
     props[COMMIT_INTERVAL_MS_CONFIG] = 0
-    props[DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG] = CustomDeserializationExceptionHandler::class.java
-    props[DEFAULT_PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG] = CustomProductionExceptionHandler::class.java
+    props[DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG] = CustomDeserializationExceptionHandler::class.java
+    props[PRODUCTION_EXCEPTION_HANDLER_CLASS_CONFIG] = CustomProductionExceptionHandler::class.java
     props[INTERCEPTOR_CLASSES_CONFIG] = interceptorClass
 
     return KafkaStreamsConfiguration(props)
