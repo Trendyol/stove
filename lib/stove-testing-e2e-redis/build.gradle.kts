109416172b2294d47b1a3ec10b6f2c6ff857b7cd
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-06-03 18:24:47 +0300

    chore: fix micrometer class not found problem after bumping lettuce-core to 6.7.0

--- lib/stove-testing-e2e-redis/build.gradle.kts
+++ lib/stove-testing-e2e-redis/build.gradle.kts
@@ -1,6 +1,9 @@
 dependencies {
-    api(projects.lib.stoveTestingE2e)
-    api(libs.lettuce.core)
-    api(libs.testcontainers.redis)
+  api(projects.lib.stoveTestingE2e)
+  api(libs.lettuce.core)
+  api(libs.testcontainers.redis)
+
+  // Reason: https://github.com/redis/lettuce/issues/3317
+  api(libs.micrometer.commons)
 }
 
