56a192177c355a942d689f809afa3507b1e8d1d1
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-06-04 14:47:50 +0300

    remove micrometer-commons as lettuce-core fixed the problem

--- lib/stove-testing-e2e-redis/build.gradle.kts
+++ lib/stove-testing-e2e-redis/build.gradle.kts
@@ -2,8 +2,5 @@ dependencies {
   api(projects.lib.stoveTestingE2e)
   api(libs.lettuce.core)
   api(libs.testcontainers.redis)
-
-  // Reason: https://github.com/redis/lettuce/issues/3317
-  api(libs.micrometer.commons)
 }
 
