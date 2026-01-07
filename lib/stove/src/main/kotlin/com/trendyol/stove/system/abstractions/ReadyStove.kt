331689bd2dc6245826647015f4c34742eea79f5a
Oguzhan Soykan <oguzhansoykan@gmail.com>
2026-01-07 21:00:22 +0100

    chore: kdoc

--- lib/stove/src/main/kotlin/com/trendyol/stove/system/abstractions/ReadyStove.kt
+++ lib/stove/src/main/kotlin/com/trendyol/stove/system/abstractions/ReadyStove.kt
@@ -1,7 +1,7 @@
 package com.trendyol.stove.system.abstractions
 
 /**
- * Marks the [TestSystem] as ready after it is started.
+ * Marks the [com.trendyol.stove.system.Stove] as ready after it is started.
  * @author Oguzhan Soykan
  */
 interface ReadyStove {
